bd2fd48693a8f583484cecb8f50ec2f096a10951
Bharatdeep Hazarika <67242026+Bajrangi18@users.noreply.github.com>
2022-02-23 23:49:55 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,45 +1,10 @@
-
-    // function Log(msg){
-    // document.querySelector('#logs').innerHTML =
-    //                 document.querySelector('#logs').innerHTML + "<br>" + msg
-    //
-    // }
-
-    var myCharacteristic;
+var myCharacteristic;
     const btn = document.getElementById("btn");
     const text = document.getElementById("logs");
 
   btn.addEventListener("click", async event =>{
-        navigator.bluetooth.requestDevice({
-           acceptAllDevices: true,
-           optionalServices: ['4fafc201-1fb5-459e-8fcc-c5c9c331914b']
-       })
-       .then(device => {
-            log('Connecting to GATT Server...' + device.name );
-          return device.gatt.connect();
-       })
-      .then(server => {
-            log('Getting Service...');
-         return server.getPrimaryService("4fafc201-1fb5-459e-8fcc-c5c9c331914b");
-       })
-      .then(service => {
-            log('Getting Characteristic...');
-            return service.getCharacteristic("beb5483e-36e1-4688-b7f5-ea07361b26a8");
-       })
-      .then(characteristic => {
-         myCharacteristic = characteristic;
-          return myCharacteristic.startNotifications().then(_ => {
-          log('> Notifications started');
-          myCharacteristic.addEventListener('characteristicvaluechanged',
-          handleNotifications);
-          });
-       })
-      .catch(error => {
-           log(error);
-      });
-     
-
-  })
+        startButton();
+   })
 
   function handleNotifications(event) {
   let value = event.target.value;
@@ -56,7 +21,33 @@ function log(data) {
   text.innerHTML = text.innerHTML + "<br>" + data;
 }
 
-// navigator.bluetooth.requestDevice({
-//             acceptAllDevices: true,
-//             optionalServices: ['4fafc201-1fb5-459e-8fcc-c5c9c331914b']
-//         })
+
+function startButton() {
+  navigator.bluetooth.requestDevice({
+     acceptAllDevices: true,
+     optionalServices: ['4fafc201-1fb5-459e-8fcc-c5c9c331914b']
+  })
+  .then(device => {
+      log('Connecting to GATT Server...' + device.name );
+    return device.gatt.connect();
+  })
+  .then(server => {
+      log('Getting Service...');
+   return server.getPrimaryService("4fafc201-1fb5-459e-8fcc-c5c9c331914b");
+  })
+  .then(service => {
+      log('Getting Characteristic...');
+      return service.getCharacteristic("beb5483e-36e1-4688-b7f5-ea07361b26a8");
+  })
+  .then(characteristic => {
+   myCharacteristic = characteristic;
+    return myCharacteristic.startNotifications().then(_ => {
+    log('> Notifications started');
+    myCharacteristic.addEventListener('characteristicvaluechanged',
+    handleNotifications);
+    });
+  })
+  .catch(error => {
+     log(error);
+  });
+}
